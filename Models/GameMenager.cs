54572fddd4ed7a0a37e3d08948fd129d4f0adbdf
rhinci <dashalupanova@gmail.com>
2025-12-05 09:52:17 +1000

    заглушка gameManager

---
+++ Models/GameMenager.cs
@@ -0,0 +1,18 @@
+﻿using SeaBattle.Enums;
+using SeaBattle.Models;
+
+namespace SeaBattle.Models
+{
+    public class GameManager
+    {
+
+        private GameState _gameState;
+        private GameBoard _myBoard;
+        private GameBoard _enemyBoard;
+
+        public GameManager()
+        {
+            // заглушка конструктора
+        }
+    }
+}
